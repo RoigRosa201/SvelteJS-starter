0718763a6f6e2983fb30896ee5a44cd2f66dc708
RoigRosa201 <roigrosa88@gmail.com>
2019-05-31 04:32:28 -0400

    first commit, basic hello world on svelt and some commentary.

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	// export let name;
+	// export let age;
+	let name = 'Roig';
+	let age = 29
+
+	function incrementAge() {
+		age += 1;
+	}
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+
+<button on:click="{incrementAge}">Change age</button>
+
+<!-- Quotation marks are not required above, they are for helping to highlight syntax. -->
