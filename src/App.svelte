b1eeb3d695ecb220a19f6bea53e32d46d57abad9
RoigRosa201 <roigrosa88@gmail.com>
2019-06-01 13:39:55 -0400

    added dynamic reclaculation and labelde statement, reactive vatriable also added.

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,20 @@
 	let name = 'Roig';
 	let age = 29
 
+	// let uppercaseName; not required;
+
+	$: uppercaseName = name.toUpperCase();
+		// labeled statment and dynamic recalculation
+	
+	
 	function incrementAge() {
 		age += 1;
+	}
+
+	function changeName() {
+		name = 'Roigus';
+
+
 	}
 
 </script>
@@ -16,8 +28,9 @@
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
-
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<!-- <h1>Hello {name.UpperCase}, my age is {age}!</h1> also optional-->
 <button on:click="{incrementAge}">Change age</button>
+<button on:click="{changeName}">Change Name</button>
 
 <!-- Quotation marks are not required above, they are for helping to highlight syntax. -->
